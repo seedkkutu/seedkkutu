0b531cc3d737b00cb535719663466b1bf9eb480e
1ongames <114791859+1ongames@users.noreply.github.com>
2024-10-24 19:35:46 +0900

    Update admin.js

--- Server/lib/Web/routes/admin.js
+++ Server/lib/Web/routes/admin.js
@@ -100,6 +100,25 @@ Server.get("/gwalli/kkutudb/:word", function(req, res){
 		res.send($doc);
 	});
 });
+// 제발 정상작동해라
+Server.delete("/gwalli/deletedb/:word", function(req, res){
+if(!checkAdmin(req, res)) return;
+if(req.body.pw != GLOBAL.PASS) return res.sendStatus(400);
+        
+var TABLE = MainDB.kkutu[req.query.lang];
+        
+if(!TABLE) return res.sendStatus(400);
+if(!TABLE.remove) return res.sendStatus(400);
+
+	TABLE.remove([ '_id', req.params.word ]).on(function($res){
+        	if($res) {
+        	JLog.info(`Word '${req.params.word}' deleted by ${req.ip}`);
+        		res.send({ success: true, message: `Word '${req.params.word}' deleted.` });
+        	} else {
+        		res.sendStatus(404);
+            	}
+        });
+});
 Server.get("/gwalli/kkututheme", function(req, res){
 	if(!checkAdmin(req, res)) return;
 	
